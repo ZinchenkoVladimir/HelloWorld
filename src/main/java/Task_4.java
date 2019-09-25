8bba83f7f4bd0c59e725dace41ff82ab1b569eec
VolodymyrZinchenko <galdrastav@gmail.com>
2019-09-25 18:38:06 +0300

    1

---
+++ src/main/java/Task_4.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class Task_4 {
+
+    public static float a = 5;
+    public static float b = 10;
+
+    public static void main (String[] args) {
+
+        float result = a/b;
+
+        System.out.println(result);
+    }
+}
