daa19d30a5b550d6b8940cb175626b1b619a02b8
VolodymyrZinchenko <galdrastav@gmail.com>
2019-09-25 17:03:25 +0300

    1

---
+++ src/main/java/Hello_World.java
@@ -0,0 +1,22 @@
+package main.java;
+
+public class Hello_World {
+
+    private String hello_world = "Hello World";
+
+    public String getHello_world() {
+        return hello_world;
+    }
+
+    public static void main(String[] args){
+
+        Hello_World h_w = new Hello_World();
+        h_w.getHello_world();
+
+        System.out.println(h_w.getHello_world());
+        System.out.println(h_w.getHello_world());
+        System.out.println(h_w.getHello_world());
+        System.out.println(h_w.getHello_world());
+        System.out.println(h_w.getHello_world());
+    }
+}
