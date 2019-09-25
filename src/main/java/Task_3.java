f06a3c18d71d63f95961cc8c58632f7332579977
VolodymyrZinchenko <galdrastav@gmail.com>
2019-09-25 18:31:45 +0300

    1

---
+++ src/main/java/Task_3.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class Task_3 {
+
+    public static void main (String[] args) {
+
+        int a = 100500;
+        int b = 777;
+        int c;
+
+        c = a*b;
+
+        System.out.println(c);
+    }
+}
