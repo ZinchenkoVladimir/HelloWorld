68bec664399816c155a41e0a61dd2694ea51cac4
VolodymyrZinchenko <galdrastav@gmail.com>
2019-09-25 19:05:31 +0300

    1

---
+++ src/main/java/Task_6.java
@@ -0,0 +1,18 @@
+package main.java;
+
+public class Task_6 {
+
+    public static void main (String[] args) {
+
+//        String s = "Java";
+//        System.out.println("Happy");
+//        System.out.println("Java Learning");
+//        System.out.println("programming");
+        System.out.print("Happy Java");
+//        System.out.println("wekend");
+//        System.out.println(s);
+//        System.out.print("Python");
+        System.out.print(" ");
+        System.out.println("Learning");
+    }
+}
