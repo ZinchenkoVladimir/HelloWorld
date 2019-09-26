ed44e626abc10594017cf8cd12215ded1b420bc1
VolodymyrZinchenko <galdrastav@gmail.com>
2019-09-26 12:22:01 +0300

    1

---
+++ src/main/java/Task_7.java
@@ -0,0 +1,19 @@
+package main.java;
+
+public class Task_7 {
+
+    public static void main (String[] args) {
+
+        int a = 10;
+        int b = 15;
+        double c = b + 38;
+
+        String s = "s" + a;
+        String s1 = a + "b";
+//        String s2 = "a";
+        String s3 = s1 + "a";
+        String s4 = s3 + "b";
+
+        System.out.println(c + s4 + s);
+    }
+}
